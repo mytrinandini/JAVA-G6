9d2041a261e0483f24a33dab7e55ccd087de97fd
mytrinandini <mythrisainandini@gmail.com>
2026-07-11 15:13:25 +0530

    Delete ArrayEx.java

--- ArrayEx.java
+++
@@ -1,9 +0,0 @@
-class ArrayEx{
-      public static void main(String args[]){
-      String[]name=new String[4];
-             name[0]="spoorthi";
-              name[1]="praveena";
-              name[2]="nandini";
-              System.out.println(name[1]);
-              }
-}
