81e78473ddb422c1eab39f2e16e7373b46357844
mytrinandini <mythrisainandini@gmail.com>
2026-07-11 15:10:29 +0530

    Add files via upload

---
+++ ArrayEx.java
@@ -0,0 +1,9 @@
+class ArrayEx{
+      public static void main(String args[]){
+      String[]name=new String[4];
+             name[0]="spoorthi";
+              name[1]="praveena";
+              name[2]="nandini";
+              System.out.println(name[1]);
+              }
+}
