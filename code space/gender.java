ea6b452edb7a47b089b2c4e3cc4483537c400153
mytrinandini <mythrisainandini@gmail.com>
2026-07-11 16:18:32 +0530

    Add files via upload

---
+++ code space/gender.java
@@ -0,0 +1,8 @@
+public class gender{
+public static void main(String args[]){
+char gender='M';
+String gender2="F";
+System.out.println("gender"+gender);
+System.out.println("gender"+gender2);
+}
+}
