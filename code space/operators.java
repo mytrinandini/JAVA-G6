8c8163077e86993ff31b683c030f8f812f5f207b
mytrinandini <mythrisainandini@gmail.com>
2026-07-11 15:56:23 +0530

    Add files via upload

---
+++ code space/operators.java
@@ -0,0 +1,11 @@
+public class operators{
+public static void main(String args[]){
+int a=12;
+int b=10;
+System.out.println("Bitwise AND:"+(a&b));
+System.out.println("Bitwise OR:"+(a|b));
+System.out.println("Bitwise XOR:"+(a^b));
+int c=4;
+System.out.println("Bitwise NOT:"+(~c));
+}
+}
