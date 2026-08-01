71da49e6e6dbcdb4c05d6d073372b417a768f876
mytrinandini <mythrisainandini@gmail.com>
2026-08-01 15:35:56 +0530

    Add files via upload

---
+++ code space/ScopeEx.java
@@ -0,0 +1,17 @@
+class ScopeEx{
+public static void main(String args[]){
+String name="nandini";
+int age = 18;
+System.out.println(name);
+System.out.println(age+"years old");
+if(age>=18){
+String status="Allowed";
+System.out.println(status);
+}
+else{
+String status = "Not allowed";
+System.out.println(status);
+}
+}
+}
+
