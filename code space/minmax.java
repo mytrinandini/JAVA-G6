b8bc8ab7e1686aa03be3322b536066b17939f553
mytrinandini <mythrisainandini@gmail.com>
2026-08-02 21:19:36 +0530

    Add files via upload

---
+++ code space/minmax.java
@@ -0,0 +1,8 @@
+public class minmax{
+public static void main(String args[]){
+int Minval=Integer.MIN_VALUE;
+System.out.println(Minval);
+int Maxval=Integer.MAX_VALUE;
+System.out.println(Maxval);
+}
+}
