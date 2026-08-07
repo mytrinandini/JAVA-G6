f3b33ae701c15d8a7ba82c83f8ef936738e3cddf
mytrinandini <mythrisainandini@gmail.com>
2026-08-07 15:35:48 +0530

    Add files via upload

---
+++ code space/encaps.java
@@ -0,0 +1,29 @@
+class main {
+    private String name;
+    private int marks;
+    public String getname() {
+        return name;
+    }
+    public int getmarks() {
+        return marks;
+    }
+    public void setname(String name) {
+        this.name = name;
+    }
+    public void setmarks(int marks) {
+        if (marks >= 0 && marks <= 100) {
+            this.marks = marks;
+        } else {
+            System.out.println("Invalid marks");
+        }
+    }
+}
+public class encaps {
+    public static void main(String[] args) {
+        main obj = new main();
+        obj.setname("nandini");
+        obj.setmarks(90); 
+        System.out.println("Name: " + obj.getname());
+        System.out.println("Marks: " + obj.getmarks());
+    }
+}
