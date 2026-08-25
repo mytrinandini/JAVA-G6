25d09ffbac5b103097fdeb4823a871f4c9740550
mytrinandini <mythrisainandini@gmail.com>
2026-08-25 16:14:10 +0530

    Add files via upload

---
+++ code space/PrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataTypes {
+public static void main(String[] args) {
+byte byteValue = 100;
+short shortValue = 10000;
+int intValue = 100000;
+long longValue = 10000000000L;
+float floatValue = 10.5f;
+double doubleValue = 20.12345;
+char charValue = 'A';
+boolean booleanValue = true;
+        System.out.println("byte: " + byteValue);
+        System.out.println("short: " + shortValue);
+        System.out.println("int: " + intValue);
+        System.out.println("long: " + longValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("char: " + charValue);
+        System.out.println("boolean: " + booleanValue);
+    }
+}
